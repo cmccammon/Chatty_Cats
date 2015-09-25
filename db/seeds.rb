ba6e4081f530a51fcf546198868473b58118101c
Chris McCammon <cmccammon@gmail.com>
2015-09-25 12:29:32 -0500

    seeds are done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+
+10.times do
+ user = User.create({
+   name: Faker::Name.first_name,
+   last_name: Faker::Name.last_name,
+   age: Faker::Number.between(18, 85)
+   })
+end
+5.times do
+  room = Room.create({
+    name: Faker::N.between(1, 50),
+    })
+end
+25.times do
+  chats = Chat.create({
+    messages: Faker::Hacker.say_something_smart,
+    user_id: Faker::Number.between(1, 10),
+    room_id: Faker::Number.between(1, 5)
+
+
+    })
+
+
+end
