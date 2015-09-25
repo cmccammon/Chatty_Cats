b553527f51fbc5400b4c9a72df638c1e1274d488
Chris McCammon <cmccammon@gmail.com>
2015-09-25 14:40:58 -0500

    updated cors for security

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-
   def index
     users = User.all
     render json: users.to_json, status: 200
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   resources :users
   resources :chats
   root 'rooms#index'
+  match '*not_found_route', to: 'application#not_found', via: [:get, :post, :put, :delete]
 end
